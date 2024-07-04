6f64f41d8b92bb99fa49a127505a170c5c511e55
Brian Harper <bharper@gmail.com>
2024-07-04 14:16:19 -0500

    init

---
+++ modify_podcast.py
@@ -0,0 +1,154 @@
+import warnings
+#warnings.filterwarnings("ignore", category=UserWarning, module='torchaudio._backend')
+warnings.filterwarnings("ignore", category=UserWarning)
+
+import os
+import sys
+import json
+import math
+import torch
+import numpy as np
+from run_ffmpeg import main as ffmpeg
+from run_ffmpeg import get_media_duration
+from pydub import AudioSegment
+
+
+
+from pyannote.audio import Pipeline, Audio
+
+
+def get_max_speaker(j, verbose=False):
+    max_time = -1
+    max_speaker = ""
+    for k in j:
+        ar = j[k]
+        diffs = [ x[1] - x[0] for x in ar ]
+        total = sum(diffs)
+        if verbose:
+            print (f"{k} total seconds: {total}")
+        if total > max_time:
+            max_time = total
+            max_speaker = k
+
+    if verbose:
+        print (f"Max speaker: {max_speaker}")
+    return max_speaker
+
+def stitch_audio_segments(time_array, input_wav_file, output_wav_file):
+    audio = AudioSegment.from_wav(input_wav_file)
+    output_audio = AudioSegment.empty()
+    for start_time, end_time in time_array:
+        # Convert times to milliseconds
+        start_ms = int(start_time * 1000)
+        end_ms = int(end_time * 1000)
+        
+        # Extract the segment
+        segment = audio[start_ms:end_ms]
+        
+        # Append the segment to the output audio
+        output_audio += segment
+    
+    # Export the stitched audio to a new WAV file
+    output_audio.export(output_wav_file, format="wav")
+
+
+def get_speakers(input_file, write_output="speakers.json", verbose=False):
+    io = Audio(mono='downmix', sample_rate=16000)
+    waveform, sample_rate = io(input_file)
+
+    pipeline = Pipeline.from_pretrained(
+        "pyannote/speaker-diarization-3.1",
+        use_auth_token=open('hftoken.txt').read().replace('\n', ''))
+
+    pipeline.to(torch.device("cuda"))
+
+    diarization = pipeline({"waveform": waveform, "sample_rate": sample_rate})
+
+    #diarization = pipeline(input_file)
+    ar = {}
+    for turn, _, speaker in diarization.itertracks(yield_label=True):
+        if verbose:
+            print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
+        if speaker in ar:
+            ar[speaker].append([turn.start, turn.end])
+        else:
+            ar[speaker] = [[turn.start, turn.end]]
+
+    if write_output:
+        with open(write_output, 'w') as f:
+            json.dump(ar, f, indent=4)
+    return ar
+
+def convert_input(input_file, output_file):
+    ffmpeg(["-i", input_file, "-ar", "16000", "-ac", "1", output_file])
+
+def convert_audio_wav(input_file, output_file):
+    ffmpeg(["-i", input_file, output_file])
+
+def convert_audio_final(input_file, output_file):
+    ffmpeg(["-i", input_file, output_file])
+
+def get_speaker_times(speakers, threshold=60, verbose=False):
+    results = []
+    for k in speakers:
+        ar = speakers[k]
+        diffs = [ x[1] - x[0] for x in ar ]
+        total = sum(diffs)
+        if verbose:
+            print (f"{k} total seconds: {total}")
+        if total > threshold:
+            if verbose:
+                print (f"Adding speaker {k} to result times")
+            results += ar
+    results = sorted(results, key=lambda x: x[0])
+    return results
+
+def get_max_speaker_times(speakers): # alternative strategy
+    max_speaker = get_max_speaker(speakers)
+    results = speakers[max_speaker]
+    results = sorted(results, key=lambda x: x[0])
+    return results
+
+def get_dynamic_threshold(input_file):
+    duration = get_media_duration(input_file)
+    per_ten_minutes = 61
+    buffer = 10
+    # 61 seconds per every 10 minutes with a 10 second buffer
+    threshold = int((math.ceil(duration/60/10) * 61) + 10)
+    return threshold
+
+def main():
+    args = sys.argv[1:]
+    if len(args) == 0:
+        print ("Arguments missing.")
+        sys.exit(1)
+    
+    input_file = os.path.basename(args[0])
+    input_file_ext = os.path.splitext(input_file)[1]
+    if not input_file_ext.startswith("."):
+        input_file_ext = f".{input_file_ext}"
+    
+    if not os.path.exists(input_file):
+        print (f'File not found "{input_file}"')
+        sys.exit(1)
+
+    #temporary_file = "tempfile.wav"
+    hq_temporary_file = "hq_tempfile.wav"
+    hq_combined_file = "hq_combined.wav"
+    basename = os.path.basename(os.path.splitext(input_file)[0])
+    final_output_file = f"output_{basename}{input_file_ext}"
+    print (f'Input file: "{input_file}" - Output file: "{final_output_file}"')
+    print("converting file to correct format")
+    #convert_input(input_file, temporary_file)
+    convert_audio_wav(input_file, hq_temporary_file)
+    print ("Getting speakers from audio (may take some time)", flush=True)
+    speakers = get_speakers(input_file)
+    dynamic_threshold = get_dynamic_threshold(hq_temporary_file)
+    print (f"Threshold speaker time: {dynamic_threshold}")
+    speaker_times = get_speaker_times(speakers, dynamic_threshold, True)
+    stitch_audio_segments(speaker_times, hq_temporary_file, hq_combined_file)
+    convert_audio_final(hq_combined_file, final_output_file)
+    print (f"Audio output to file \"{final_output_file}\"")
+
+if __name__ == "__main__":
+    main()
---
+++ run_ffmpeg.py
@@ -0,0 +1,93 @@
+import re
+import os
+import sys
+import math
+try:
+    from alive_progress import alive_bar
+except:
+    from fake_progress import LineUpdate as alive_bar
+
+from subprocess import Popen, PIPE, DEVNULL, run, CalledProcessError
+
+def convert_time_to_seconds(time_str):
+    """
+    Convert a time string in the format HH:MM:SS.sss to seconds.
+    """
+    try:
+        hours, minutes, seconds = time_str.split(":")
+        total_seconds = (int(hours) * 3600) + (int(minutes) * 60) + float(seconds)
+        return total_seconds
+    except ValueError:
+        return 0
+
+def get_media_duration(file_path):
+    """
+    Get the duration of a media file using ffmpeg.
+    """
+    try:
+        result = run(f"ffprobe \"{file_path}\"", shell=True, check=True, text=True, capture_output=True)
+        duration_match = re.search(r'Duration: (\d{2}:\d{2}:\d{2}\.\d{2})', result.stderr)
+        if duration_match:
+            return convert_time_to_seconds(duration_match.group(1))
+        else:
+            return 0
+    except CalledProcessError as e:
+        return f"Error: {e}"
+
+def main(args):
+
+    verbose = False
+    if "DEBUG" in os.environ.keys() and os.environ["DEBUG"] == 1:
+        verbose = True
+
+    input_filename = ""
+    output_filename = ""
+    duration = 0
+    args.insert(0, 'ffmpeg')
+    if "-y" not in args:
+        args.append("-y")
+    if verbose: print (f"> {' '.join(args)}")
+    process = Popen(args, stdout=PIPE, stderr=PIPE, stdin=PIPE, text=True)
+    for line in iter(process.stderr.readline, ''):
+        m = re.match(r'\s*Duration: (\d\d:\d\d:\d\d.\d\d)', line)
+        if m:
+            duration = int(math.ceil(convert_time_to_seconds(m[1])))
+            if duration and input_filename and output_filename:
+                break
+        im = re.match("Input #\d,.*from '(.*)'", line)
+        if im:
+            if input_filename:
+                input_filename = f"{input_filename},{im[1]}"
+            else:
+                input_filename = im[1]
+
+            if duration and input_filename and output_filename:
+                break
+        om = re.match("Output #0,.*to '(.*)'", line)
+        if om:
+            output_filename = om[1]
+            if duration and input_filename and output_filename:
+                break
+        # if duration == 0:
+        #     print (line)
+        #print (duration, input_filename, output_filename)
+        
+    
+    title = f'Converting "{input_filename}" to "{output_filename}"'
+    with alive_bar(duration, manual=True, title=title) as bar:
+        for line in iter(process.stderr.readline, ''):
+            m = re.match('.*time=(\d\d:\d\d:\d\d\.\d+)', line)
+            if m:
+                time = m[1]
+                seconds = convert_time_to_seconds(time)
+                per = seconds/duration
+                bar(per)
+        bar(1)
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if len(args) == 0:
+        print ("Arguments missing.")
+        sys.exit(1)
+    
+    main(args)
